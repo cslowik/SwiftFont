ae7ef94cda078ed12daf9f19c0b815bebfe7f24f
Chris Slowik <chris@creativeda.sh>
2015-12-08 07:54:00 -0800

    Initial Commit

---
+++ Swift Font/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Font
+//
+//  Created by Chris Slowik on 12/8/15.
+//  Copyright © 2015 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
